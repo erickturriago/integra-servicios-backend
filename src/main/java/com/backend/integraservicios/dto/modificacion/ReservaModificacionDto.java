894d772484321d16b4796489d36683988ef50cea
Erick <turriago-erick@hotmail.com>
2024-05-28 02:04:07 -0500

    Update back

--- src/main/java/com/backend/integraservicios/dto/modificacion/ReservaModificacionDto.java
+++ src/main/java/com/backend/integraservicios/dto/modificacion/ReservaModificacionDto.java
@@ -27,14 +27,6 @@ public class ReservaModificacionDto {
     @NotBlank(message = "El estado de la reserva no puede estar vacío")
     private String estado;
 
-    @NotNull(message = "La fecha de inicio no puede ser nula")
-    @FutureOrPresent(message = "La fecha de inicio debe ser hoy o en el futuro")
-    private LocalDate fechaInicio;
-
-    @NotNull(message = "La fecha de fin no puede ser nula")
-    @FutureOrPresent(message = "La fecha de fin debe ser hoy o en el futuro")
-    private LocalDate fechaFin;
-
     @NotBlank(message = "La hora de inicio no puede estar vacía")
     @Pattern(regexp = "^([0-1][0-9]|2[0-3]):[0-5][0-9]$", message = "La hora de inicio debe tener el formato HH:mm")
     private String horaInicio;
